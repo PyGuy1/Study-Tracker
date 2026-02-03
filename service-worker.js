ac555c3eeec102af638c9fcf1941f89a48a7e5a8
PyGuy1 <153994805+PyGuy1@users.noreply.github.com>
2026-02-04 00:59:49 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const STATIC_CACHE = "study-tracker-static-v2";
+const OFFLINE_URL = "/index.html";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/Study-tracker.png",
+  "/Study-tracker-png.png",
+  "/Study-Tracker-poster.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== STATIC_CACHE)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+      );
+    })
+  );
+});
